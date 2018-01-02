c6a83f25330eb02396593b38d2e38956f64f381d
Joel Gonzales <joelengt@gmail.com>
2018-01-01 23:23:23 -0500

    Basic routering

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,16 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import Humans from './components/humans'
+import Fruits from './components/fruits'
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+const Gist = ({ match }) => (
+  <div>
+    <p>URL: { match.url } </p>
+    <p>ID:  { match.params.id }</p>
+  </div>
+)
 
 class Application extends React.Component {
   render() {
-    return <div>
-      <h1>Welcome, Joel a ReactJS!</h1>
-    </div>;
+    return (
+      <Router>
+        <div>
+          <div>
+            <Link to='/step/12'>
+              Navigate step1
+            </Link>
+          </div>
+          <div>
+            <Link to='/step/11'>
+              Navigate step2
+            </Link>
+          </div>
+          <div>
+            <Link to="/humans">humans</Link>
+          </div>
+          <Link to="/fruits">fruits</Link>
+          <li><Link to="/topics">Topics</Link></li>
+          <div>
+            <Switch>
+              <Route exact path='/humans' component={Humans} />
+              <Route exact path='/fruits' component={Fruits} />
+              <Route exact path='/step/:id' component={Gist} />
+              <Route path="/topics" component={Topics} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    )
   }
 }
+
+
 
 /*
  * Render the above component into the div#app
  */
-React.render(<Application />, document.getElementById('app'));
+ReactDOM.render(<Application />, document.getElementById('app'));
 
---
+++ app/src/js/components/fruits.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class Fruits extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <div>
+        <p>fruits!</p>
+      </div>
+    )
+  }
+}
+
+export default Fruits
